1a295df711d92681c29dacbc87308c9f67c364d5
Alief Dany <aliefseventri@gmail.com>
2021-05-11 09:35:40 +0700

    initial commit
    in this first commit, i prepare the environtment and tools to work on this small-fun todo list project. tools that i used is prettier as the formatter, eslint as the linter, and parcel as the bundler. i also added .gitignore to ignore some auto generate file/folders and node_modules

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return React.createElement("h1", {}, "Hello World");
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));

867e5b50ceabdc86781626fd041df0bf63693b2a
Alief Dany <aliefseventri@gmail.com>
2021-05-12 14:32:05 +0700

    added eslint plugin: plugin-react plugin-jsx-a11y plugin-import
    plugin-import used when we used jsx, and the import statement would have a no-unsed-vars error because eslint doesnt know yet what have to deals with that import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return React.createElement("h1", {}, "Hello World");
+  return <h1>Hello World</h1>;
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
